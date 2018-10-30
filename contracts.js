895bdadaa8f6331952a5d157b5e52bf0fd3559be
Andres Berrios <andres.berrios.j@gmail.com>
2018-10-30 19:10:34 +0100

    Format code using eslint

--- contracts.js
+++ contracts.js
@@ -1,49 +1,49 @@
-const fs = require("fs");
-const eosjs = require("eosjs");
+const fs = require('fs')
+const eosjs = require('eosjs')
 
 const keypair = {
-  public: "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV",
-  private: "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
-};
+  public: 'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV',
+  private: '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
+}
 
 const eos = eosjs({
-  httpEndpoint: "http://localhost:8888",
-  chainId: "cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f",
+  httpEndpoint: 'http://localhost:8888',
+  chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
   keyProvider: keypair.private
-});
+})
 
-module.exports = async () => {
+module.exports = async() => {
   await setContract(
-    "eosio",
-    "/Users/andresberrios/Projects/eosio/eos/contracts/eosio.bios",
-    "eosio.bios"
-  );
-  await createAccount("eosio.token");
+    'eosio',
+    '/Users/andresberrios/Projects/eosio/eos/contracts/eosio.bios',
+    'eosio.bios'
+  )
+  await createAccount('eosio.token')
   await setContract(
-    "eosio.token",
-    "/Users/andresberrios/Projects/eosio/eos/contracts/eosio.token",
-    "eosio.token"
-  );
-  await eos.transaction("eosio.token", tr => {
-    tr.create("eosio.token", "1000000000.0000 EOS");
-  });
-};
+    'eosio.token',
+    '/Users/andresberrios/Projects/eosio/eos/contracts/eosio.token',
+    'eosio.token'
+  )
+  await eos.transaction('eosio.token', tr => {
+    tr.create('eosio.token', '1000000000.0000 EOS')
+  })
+}
 
 function createAccount(name) {
   return eos.transaction(tr => {
     tr.newaccount({
-      creator: "eosio",
+      creator: 'eosio',
       name: name,
       owner: keypair.public,
       active: keypair.public
-    });
-  });
+    })
+  })
 }
 
 async function setContract(account, contractDir, contractName) {
-  const wasm = fs.readFileSync(`${contractDir}/${contractName}.wasm`);
-  const abi = fs.readFileSync(`${contractDir}/${contractName}.abi`);
+  const wasm = fs.readFileSync(`${contractDir}/${contractName}.wasm`)
+  const abi = fs.readFileSync(`${contractDir}/${contractName}.abi`)
 
-  await eos.setcode(account, 0, 0, wasm);
-  await eos.setabi(account, JSON.parse(abi));
+  await eos.setcode(account, 0, 0, wasm)
+  await eos.setabi(account, JSON.parse(abi))
 }
--- eosiocpp.js
+++ eosiocpp.js
@@ -1,15 +1,15 @@
-const { spawn } = require("child_process");
+const { spawn } = require('child_process')
 
 main().catch(error => {
-  console.error(error);
-  process.exit(1);
-});
+  console.error(error)
+  process.exit(1)
+})
 
 async function main() {
-  if (process.argv[2] === "-o") {
-    await compile(process.argv[3], process.argv[4]);
-  } else if (process.argv[2] === "-g") {
-    await generateAbi(process.argv[3], process.argv[4]);
+  if (process.argv[2] === '-o') {
+    await compile(process.argv[3], process.argv[4])
+  } else if (process.argv[2] === '-g') {
+    await generateAbi(process.argv[3], process.argv[4])
   }
 }
 
@@ -19,7 +19,7 @@ async function compile(output, input) {
     --mount type=bind,src="$(pwd)",dst=/mnt/dev/contract \
     -w "/opt/eosio/bin/" eosio/eos-dev:v1.3.0 \
     eosiocpp -o /mnt/dev/contract/${output} /mnt/dev/contract/${input}`
-  );
+  )
 }
 
 async function generateAbi(output, input) {
@@ -28,12 +28,12 @@ async function generateAbi(output, input) {
     --mount type=bind,src="$(pwd)",dst=/mnt/dev/contract \
     -w "/opt/eosio/bin/" eosio/eos-dev:v1.3.0 \
     eosiocpp -g /mnt/dev/contract/${output} /mnt/dev/contract/${input}`
-  );
+  )
 }
 
 function run(command) {
   return new Promise(resolve => {
-    const child = spawn("sh", ["-c", command], { stdio: "inherit" });
-    child.on("close", resolve);
-  });
+    const child = spawn('sh', ['-c', command], { stdio: 'inherit' })
+    child.on('close', resolve)
+  })
 }
