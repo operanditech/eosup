9d1577fc92d45f87d44acc27838fa8ac8249a70d
andresberrios <andres.berrios.j@gmail.com>
2019-03-15 15:47:00 +0100

    Add eosio.cdt compiler through Docker

---
+++ bin/compile.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const Compiler = require('../lib/compiler')
+
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
+
+async function main() {
+  const compiler = new Compiler()
+  await compiler.setup()
+  await compiler.compile(process.argv[2], process.argv[3], process.argv[4])
+}
