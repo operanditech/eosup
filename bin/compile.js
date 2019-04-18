66fba4b425a8d4d9252b57c27a3416a2ad87d513
andresberrios <andres.berrios.j@gmail.com>
2019-04-18 21:19:14 +0200

    Allow specifying eosio.cdt version Fix compilation bin extraParams

--- bin/compile.js
+++ bin/compile.js
@@ -8,9 +8,12 @@ main().catch(error => {
 })
 
 async function main() {
-  const compiler = new Compiler({ printOutput: true })
+  const compiler = new Compiler({
+    version: process.env.EOSIO_CDT_VERSION,
+    printOutput: true
+  })
   await compiler.setup()
   await compiler.compile(process.argv[2], process.argv[3], process.argv[4], {
-    extraParams: process.argv[5] || ''
+    extraParams: process.argv[5] ? process.argv.slice(5).join(' ') : ''
   })
 }
